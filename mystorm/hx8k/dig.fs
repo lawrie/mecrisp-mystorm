ddd048d0fd4033b8f043b2d8ecacdffbf7c24da4
lawrie <lawrie.griffiths@ntlworld.com>
2018-04-10 15:18:21 +0100

    Use $ for hex constants

--- mystorm/hx8k/dig.fs
+++ mystorm/hx8k/dig.fs
@@ -1,15 +1,15 @@
 : dig
  case
-  0 of f03 endof
-  1 of 3 endof
-  2 of b05 endof
-  3 of 907 endof
-  4 of 407 endof
-  5 of d06 endof
-  6 of f06 endof
-  7 of 803 endof
-  8 of f07 endof
-  9 of d07 endof
+  0 of $f03 endof
+  1 of $3 endof
+  2 of $b05 endof
+  3 of $907 endof
+  4 of $407 endof
+  5 of $d06 endof
+  6 of $f06 endof
+  7 of $803 endof
+  8 of $f07 endof
+  9 of $d07 endof
   0 swap
  endcase
  7s
